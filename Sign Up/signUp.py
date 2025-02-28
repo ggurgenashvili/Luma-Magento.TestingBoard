b78e76504078a275149106c17c0bb8ed22fb444a
g.gurgenashvili <ggurgenashvili1@gmail.com>
2025-02-28 12:07:57 +0400

    added sign up

--- Sign Up/signUp.py
+++ Sign Up/signUp.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
+#in this test case we validate that with correct information user can create an account
 firstname = 'Test'
 lastname = 'Test2'
-email = 'test88@gmail.com'
+email = 'test842@gmail.com'
 password = 'Password@1'
 confirm_password = 'Password@1'
 
 browser = webdriver.Firefox()
 browser.get('https://magento.softwaretestingboard.com/')
 
-#navigatre to Create an Account page
+#navigate to Create an Account page
 browser.find_element(By.XPATH,'//a[text()="Create an Account"]').click()
 browser.find_element(By.ID, 'firstname').send_keys(firstname)
 browser.find_element(By.ID, 'lastname').send_keys(lastname)
